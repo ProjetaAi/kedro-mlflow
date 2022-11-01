2164d2bb366ac1b1b0808278e0b6fd87309bba48
nickolasrm <nickolasrochamachado@gmail.com>
2022-11-01 01:36:14 -0300

    added model logger partitioned dataset
    Signed-off-by: nickolasrm <nickolasrochamachado@gmail.com>

---
+++ kedro_mlflow/io/models/mlflow_partitioned_model_logger_dataset.py
@@ -0,0 +1,41 @@
+from typing import Any, Dict
+
+from kedro_mlflow.io.partitioned import MlflowPartitionedDataSet
+
+
+class MlflowPartitionedModelLoggerDataSet(MlflowPartitionedDataSet):
+    """Wrapper for ``MlflowModelLoggerDataSet`` to work dynamically."""
+
+    def __init__(
+        self,
+        data_set: Dict[str, Any] = {},
+        credentials: Dict[str, Any] = None,
+        load_args: Dict[str, Any] = None,
+        parent_run_id: str = None,
+    ):
+        """Initialize the Kedro ``MlflowPartitionedModelLoggerDataSet``.
+
+        Args:
+            data_set (Dict[str, Any], optional): Underlying MlflowModelLoggerDataSet
+                configuration. Defaults to {}.
+            credentials (Dict[str, Any], optional): Credentials to access the underlying
+                filesystem. Defaults to None.
+            load_args (Dict[str, Any], optional): Additional arguments to pass to the
+                underlying dataset. Defaults to None.
+            pyfunc_workflow (str, optional): Either `python_model` or `loader_module`.
+                See https://www.mlflow.org/docs/latest/python_api/mlflow.pyfunc.html#workflows.
+            parent_run_id (str, optional): The run ID of the parent run, if not
+                specified equals to top of the active runs stack. Defaults to None.
+        """
+        assert "flavor" in data_set, "flavor must be specified in dataset"
+
+        super().__init__(
+            {
+                "type": "kedro_mlflow.io.models.MlflowModelLoggerDataSet",
+                **data_set,
+            },
+            credentials,
+            load_args,
+            parent_run_id,
+            {"save_args.registered_model_name"},
+        )
